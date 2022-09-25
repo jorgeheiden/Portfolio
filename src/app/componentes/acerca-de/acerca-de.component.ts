9f5cc179fb7f976988310337c674d478e2a16f43
jorge heidenreich <jmjhjorge@gmail.com>
2022-09-25 13:07:43 -0300

    animacion al hacer scroll implementdado en el componente acerca-de

--- src/app/componentes/acerca-de/acerca-de.component.ts
+++ src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-acerca-de',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercaDeComponent implements OnInit {
 
+  @ViewChild('elemento') elemento!:ElementRef
+
   constructor() { }
+
+  ocultar = true
+  mostrar = false
+
+  ngAfterViewInit(){
+    console.log(this.elemento.nativeElement)
+    const threshold = 0.5
+    const observer = new IntersectionObserver(
+      (entradas) => {
+        entradas.forEach( (entrada) =>{
+          if(entrada.isIntersecting){
+            this.ocultar = false
+            this.mostrar = true
+            observer.disconnect()
+
+          }
+        })
+      }, {threshold}
+    )
+    observer.observe(this.elemento.nativeElement)
+  }
 
   ngOnInit(): void {
   }
 
+  
 }
