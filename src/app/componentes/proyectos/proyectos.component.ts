f9207a6e50bbd6135a74fa2c0440d4ed523141fd
jorge heidenreich <jmjhjorge@gmail.com>
2022-09-26 18:32:15 -0300

    Animcaion scroll. componente proyectos

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
+  ocultar = true
+  mostrar = false
+
+  @ViewChild('proyecto') proyecto!:ElementRef
+
   constructor() { }
+
+  ngAfterViewInit(){
+    const threshold = 1
+    const observer = new IntersectionObserver(
+      (entradas) =>{
+        entradas.forEach( entrada => {
+          if (entrada.isIntersecting){
+            this.ocultar = false
+            this.mostrar = true
+            observer.disconnect()
+          }
+        })
+      }, {threshold}
+    )
+    observer.observe(this.proyecto.nativeElement)
+  }
 
   ngOnInit(): void {
   }
